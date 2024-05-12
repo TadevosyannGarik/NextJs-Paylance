97e3d04f7c8cb68397e0df556caca757a84676f9
TadevosyannGarik <tadevosyan12@gmail.com>
2024-05-12 20:09:35 +0400

    Data Table Component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -14,6 +14,8 @@ const buttonVariants = cva("inline-flex items-center justify-center whitespace-n
                 secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
                 ghost: "hover:bg-accent hover:text-accent-foreground",
                 link: "text-primary underline-offset-4 hover:underline",
+                outlineDanger: "border border-red-500 text-red-500 bg-background hover:bg-accent hover:text-accent-foreground hover:text-red-500",
+
             },
             size: {
                 default: "h-10 px-4 py-2",
