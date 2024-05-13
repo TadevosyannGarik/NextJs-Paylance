e1c2446527935ea30f9afe31340d2ac1da2c6d8b
TadevosyannGarik <tadevosyan12@gmail.com>
2024-05-13 17:11:02 +0400

    Accounts Edit

---
+++ features/accounts/api/use-edit-account.ts
@@ -0,0 +1,33 @@
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType } from "hono";
+import { toast } from "sonner";
+
+
+type ResponseType = InferRequestType<typeof client.api.accounts[":id"]["$patch"]>;
+type RequestType = InferRequestType<typeof client.api.accounts[":id"]["$patch"]>["json"]; 
+
+export const useEditAccount = (id?: string) => {
+    const queryClient = useQueryClient();
+    
+    const mutation = useMutation<ResponseType, Error, RequestType>({
+        // @ts-ignore
+        mutationFn: async (json) => {
+            const response = await client.api.accounts[":id"]["$patch"]({ 
+                json,
+                param: {id} 
+            });
+            return await response.json();
+        },
+        onSuccess: () => {
+            toast.success("Account updated");
+            queryClient.invalidateQueries({ queryKey: ["accounts", { id }] });
+            queryClient.invalidateQueries({ queryKey: ["accounts"] });
+        },
+        onError: () => {
+            toast.error("Failed to edit account");
+        },
+    });
+
+    return mutation;
+}
